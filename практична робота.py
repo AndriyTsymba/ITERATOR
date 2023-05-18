0973a23840a0c8121952ea524f894bb0baa35b0d
student <a.tsymbaliuk2007@gmail.com>
2023-05-18 20:45:18 +0300

    3 завдання

--- практична робота.py
+++ практична робота.py
@@ -1,29 +1,21 @@
-class OddIterator:
-    def __init__(self, N):
-        if N <= 0:
-            raise ValueError
-        self.N = N
-        self.current = 1
+#3
+lst= [1,2,3,4,5,6,6]
+print(iter(lst))
+class ListIterator:
+    def __init__(self,date):
+        self.date = date
+        self.index = 0
     def __iter__(self):
         return self
     def __next__(self):
-        if self.current > self.N:
-            raise StopIteration
-        if self.current % 2 != 0:
-            number = self.current
-            self.current += 2
-            return number
-        else:
-            self.current += 1
-        return self.__next__()
-try:
-    iterator = OddIterator(10)
-    for number in iterator:
-        print(number)
-except ValueError as e:
-        print(str(e))
-except StopIteration:
-        print("Кінець")
+        if self.index >= len(self.date):
+            raise ListIterator
+        value =self.date[self.index]
+        self.index += 1
+        return value
+for num in ListIterator(lst):
+     print(num)
+
 
 
 
