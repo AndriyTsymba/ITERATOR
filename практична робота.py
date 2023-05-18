bfb0d7607604df7d6a9c07f6a9344f0491efbe2d
student <a.tsymbaliuk2007@gmail.com>
2023-05-18 20:52:23 +0300

    4 завдання

--- практична робота.py
+++ практична робота.py
@@ -1,20 +1,20 @@
-#3
-lst= [1,2,3,4,5,6,6]
-print(iter(lst))
-class ListIterator:
-    def __init__(self,date):
-        self.date = date
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.date):
-            raise ListIterator
-        value =self.date[self.index]
-        self.index += 1
-        return value
-for num in ListIterator(lst):
-     print(num)
+#4
+def multiply_by():
+    def mult(n):
+        if n == int(n):
+            return n*n
+        else:
+            raise TypeError('веденні дані не правильні')
+        return mult
+m = multiply_by()
+print(int(input("Введіть число")))
+print(int(input("введіть число")))
+
+
+
+
+
+
 
 
 
